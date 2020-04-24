a4df4c8e991fa9ab313e0cf873fbd41fa249bb9a
Shweta Patil <theshwetapatil@gmail.com>
2020-04-24 23:52:41 +0530

    Added Polish

---
+++ Prototype/Prototype/Assets/Scripts/GameLogic/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+   public void NextScene()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex;
+        
+        level++;
+
+        if (level >= SceneManager.sceneCountInBuildSettings)
+        {
+            level = 0;
+        }
+
+        SceneManager.LoadScene(level);
+    }
+
+    public void PrevScene()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex;
+
+        level--;
+
+        if (level <= 0)
+        {
+            level = SceneManager.sceneCountInBuildSettings - 1;
+        }
+
+        SceneManager.LoadScene(level);
+    }
+
+    public void Retry()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(level);
+    }
+
+    public void WrapTo(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
